81e1f98437cd003fd1ecb8d9c54973a2855b2ab7
emanSayma <eman89say@hotmail.com>
2017-10-28 01:40:56 +0800

    'update.txt'

--- public/js/app.js
+++ public/js/app.js
@@ -40137,7 +40137,7 @@ exports.clearImmediate = clearImmediate;
 /* 38 */
 /***/ (function(module, exports) {
 
-throw new Error("Module build failed: ModuleBuildError: Module build failed: \r\nundefined\r\n       ^\r\n      Invalid CSS after \"$body-bg:\": expected expression (e.g. 1px, bold), was \";\"\r\n      in c:\\xampp\\htdocs\\emanBlog\\resources\\assets\\sass\\_variables.scss (line 3, column 9)\n    at runLoaders (c:\\xampp\\htdocs\\emanBlog\\node_modules\\webpack\\lib\\NormalModule.js:195:19)\n    at c:\\xampp\\htdocs\\emanBlog\\node_modules\\loader-runner\\lib\\LoaderRunner.js:364:11\n    at c:\\xampp\\htdocs\\emanBlog\\node_modules\\loader-runner\\lib\\LoaderRunner.js:230:18\n    at context.callback (c:\\xampp\\htdocs\\emanBlog\\node_modules\\loader-runner\\lib\\LoaderRunner.js:111:13)\n    at Object.asyncSassJobQueue.push [as callback] (c:\\xampp\\htdocs\\emanBlog\\node_modules\\sass-loader\\lib\\loader.js:55:13)\n    at Object.<anonymous> (c:\\xampp\\htdocs\\emanBlog\\node_modules\\async\\dist\\async.js:2244:31)\n    at Object.callback (c:\\xampp\\htdocs\\emanBlog\\node_modules\\async\\dist\\async.js:906:16)\n    at options.error (c:\\xampp\\htdocs\\emanBlog\\node_modules\\node-sass\\lib\\index.js:294:32)");
+// removed by extract-text-webpack-plugin
 
 /***/ })
 /******/ ]);
